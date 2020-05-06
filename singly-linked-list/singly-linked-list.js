031c32eeec47f43ee6cf4e53bc972e52bf99c78b
Heng-Hao Pu <hpu@oswego.edu>
2020-05-06 14:17:29 -0400

    Implement pop()

--- singly-linked-list/singly-linked-list.js
+++ singly-linked-list/singly-linked-list.js
@@ -21,19 +21,44 @@ class SinglyLinkedList {
       this.head = newNode;
       this.tail = newNode;
     } else {
-      // redundant 
-      // if (!this.head.next) {
-      //   this.head.next = newNode;
-      //   this.tail = newNode;
-      // }
       this.tail.next = newNode;
       this.tail = newNode; 
     }
     this.length++;
     return this;
   }
+
+  pop() {
+    // edge case 1: no node
+    if (!this.length) return null;
+    // edge case 2: one node
+    if (this.length === 1) {
+      let temp = this.tail;
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return temp;
+    }
+    // general case
+    let current = this.head;
+    let previous = null;
+    while (current !== this.tail) {
+      previous = current;
+      current = current.next;
+    }
+    // now current points to tail, previous -> the node right before the tail
+    this.tail = previous;
+    this.tail.next = null;
+    this.length--;
+    return current;
+  }
 }
 
 const singlyLinkedList = new SinglyLinkedList();
 singlyLinkedList.push(1);
-console.log(singlyLinkedList.push(2));
+singlyLinkedList.push(2);
+singlyLinkedList.push(3);
+console.log(singlyLinkedList.pop());
+console.log(singlyLinkedList.pop());
+console.log(singlyLinkedList.pop());
+console.log(singlyLinkedList)
