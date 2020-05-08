d9c485a971ad9705937ef6ef391cbc8cf9d78722
Heng-Hao Pu <hpu@oswego.edu>
2020-05-08 15:36:10 -0400

    Implement push(val)

--- doubly-linked-list2/doubly-linked-list.js
+++ doubly-linked-list2/doubly-linked-list.js
@@ -12,4 +12,24 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.length) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail; // * extra 1 line of code from SinglyLinkedList
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
+
+const list = new DoublyLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+console.log(list)
