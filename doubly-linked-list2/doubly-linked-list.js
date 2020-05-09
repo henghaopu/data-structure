1dd7870c8eb50b95449f4998c3ffade76f5d8591
Heng-Hao Pu <hpu@oswego.edu>
2020-05-09 11:39:27 -0400

    Implement pop()

--- doubly-linked-list2/doubly-linked-list.js
+++ doubly-linked-list2/doubly-linked-list.js
@@ -26,10 +26,35 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    // edge case: no node
+    if (!this.length) return null;
+    // record the last
+    const poppingNode = this.tail;
+    if (this.length === 1) {
+      // edge case: one node
+      // null <- 1 -> null
+      this.head = null;
+      this.tail = null;
+    } else {
+      // general case: null <- 1 <-> 2 <-> 3 -> null 
+      // move the tail
+      this.tail = this.tail.prev;
+      // break the links
+      this.tail.next = null;
+      poppingNode.prev = null;
+    }
+    this.length--;
+    return poppingNode
+  }
+
+
 }
 
 const list = new DoublyLinkedList();
 list.push(1);
 list.push(2);
 list.push(3);
+console.log(list.pop())
 console.log(list)
