79d93d643df4cbc60199a1a576c6387051998bbc
Heng-Hao Pu <hpu@oswego.edu>
2020-05-12 17:20:23 -0400

    Implement push(val)

--- stack/stack.js
+++ stack/stack.js
@@ -1,6 +1,6 @@
 class Node {
-  constructor() {
-    this.val = null;
+  constructor(val) {
+    this.val = val;
     this.next = null;
   }
 }
@@ -10,4 +10,25 @@ class Stack {
     this.top = null;
     this.size = 0;
   }
+
+  // same like unshift in singly linked list
+  push(val) {
+    let newNode = new Node(val);
+    // edge case: x node
+    if (!this.size) {
+      this.top = newNode;
+    } else {
+      // edge case: 1 node & general case: above 2 nodes are the same here  
+      newNode.next = this.top;
+      this.top = newNode;
+    }
+  
+    this.size++;
+    return this.size
+  }
 }
+
+let stack = new Stack();
+console.log(stack.push(1))
+console.log(stack.push(2))
+console.log(stack.push(3))
