4672e1b8519e155e453086b2870d5dfb220f6208
Heng-Hao Pu <hpu@oswego.edu>
2020-05-18 22:42:03 -0400

    Implement  insert()

---
+++ max-binary-heap/max-binary-heap.js
@@ -0,0 +1,34 @@
+class MaxBinaryHeap {
+  heap = [];
+
+  insert(val) {
+    this.heap.push(val);
+    if (this.heap.length) {
+      let current = this.heap.length - 1;
+      while (current >= 0) {
+        let parent = Math.floor((current - 1) / 2);
+        if (this.heap[current] > this.heap[parent]) {
+          [this.heap[current], this.heap[parent]] = [
+            this.heap[parent],
+            this.heap[current],
+          ];
+          current = parent;
+        } else {
+          break;
+        }
+      }
+    }
+
+    return this.heap;
+  }
+}
+
+const mbh = new MaxBinaryHeap();
+mbh.insert(10);
+mbh.insert(5);
+mbh.insert(8);
+mbh.insert(1);
+mbh.insert(3);
+mbh.insert(2);
+mbh.insert(15);
+console.log(mbh.heap);
