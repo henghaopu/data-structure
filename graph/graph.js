05528556b3757e09fdb6781afc00abb9b88e07f4
Heng-Hao Pu <hpu@oswego.edu>
2020-05-22 14:23:16 -0400

    Implement addVertex(vertex)

--- graph/graph.js
+++ graph/graph.js
@@ -2,4 +2,16 @@ class Graph {
   constructor() {
     this.adjacencyList = {};
   }
+
+  addVertex(vertex) {
+    // prevent overwriting when the vertex already exists
+    if (!this.adjacencyList[vertex]) {
+      // add a key to the adjacency list and set the value to an empty array
+      this.adjacencyList[vertex] = [];
+    }
+  }
 }
+
+const graph = new Graph();
+graph.addVertex('Tokyo');
+console.log(graph.adjacencyList);
