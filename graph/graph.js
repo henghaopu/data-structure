26fc9a2d947cced5167836b57a1f8115bc825f37
Heng-Hao Pu <hpu@oswego.edu>
2020-05-22 15:02:13 -0400

    Implement addEdge(v1, v2)

--- graph/graph.js
+++ graph/graph.js
@@ -10,8 +10,47 @@ class Graph {
       this.adjacencyList[vertex] = [];
     }
   }
+  // undirected
+  addEdge(v1, v2) {
+    if (this.adjacencyList.hasOwnProperty(v1)) {
+      this.adjacencyList[v1].push(v2);
+    }
+    if (this.adjacencyList.hasOwnProperty(v2)) {
+      this.adjacencyList[v2].push(v1);
+    }
+  }
 }
 
 const graph = new Graph();
+graph.addVertex('The Professor');
 graph.addVertex('Tokyo');
+graph.addVertex('Rio');
+graph.addVertex('Nairobi');
+graph.addVertex('Berlin');
+graph.addVertex('Moscow');
+graph.addVertex('Denver');
+graph.addVertex('Stockholm');
+graph.addVertex('Helsinki');
+graph.addVertex('Lisbon');
+graph.addVertex('Palermo');
+graph.addVertex('Bogotá');
+graph.addVertex('Marseille');
+graph.addVertex('Manila');
+
+graph.addEdge('The Professor', 'Berlin');
+graph.addEdge('Berlin', 'Palermo');
+graph.addEdge('The Professor', 'Lisbon');
+graph.addEdge('The Professor', 'Marseille');
+
+graph.addEdge('Tokyo', 'Rio');
+graph.addEdge('Rio', 'Denver');
+graph.addEdge('Denver', 'Stockholm');
+graph.addEdge('Denver', 'Moscow');
+graph.addEdge('Denver', 'Manila');
+graph.addEdge('Moscow', 'Manila');
+
+graph.addEdge('Tokyo', 'Nairobi');
+graph.addEdge('Nairobi', 'Helsinki');
+graph.addEdge('Nairobi', 'Bogotá');
+
 console.log(graph.adjacencyList);
